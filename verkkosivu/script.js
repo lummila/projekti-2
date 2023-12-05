7082805cbffc67ad9a2ac1b69950f3bce78ab7ab
lummila <lummila.aleksi@gmail.com>
2023-12-05 13:09:15 +0200

    aleksi

--- verkkosivu/script.js
+++ verkkosivu/script.js
@@ -1,32 +1,38 @@
+"use strict";
 
-'use strict'
+const user_username = document.querySelector(".query-user");
+const user_password = document.querySelector(".query-pass");
 
+async function getLogin(username) {
+  try {
+    username = user_username.value;
+    password = user_password.value;
 
-async function getLogin(username){
-  try{
-    const response = await fetch('http://localhost:5000/register');
+    const response = await fetch(
+      `http://localhost:5000/register?username=${username}&password=${password}`
+    );
     const json = response.json();
     console.log(json.name);
     return json.name;
-  } catch (error){
+  } catch (error) {
     console.error("doLogin fail", error);
   }
 }
 
-async function doLogin(event){
+async function doLogin(event) {
   event.preventDefault();
-  const username = document.getElementById('query').value;
-  const response= await getLogin(username);
+  const username = document.getElementById("query").value;
+  const response = await getLogin(username);
   const response2 = await getPassword(password);
-    if (response){
-      console.log(response);
-      console.log("success");
-      return (response)
-  } else{
+  if (response) {
+    console.log(response);
+    console.log("success");
+    return response;
+  } else {
     console.log("some error");
-    return (response.error);
+    return response.error;
   }
 }
 //Ongelma: miten saada formien molemmat arvot syötettyä samalle reitille?
-const username = document.querySelector('form')[0].addEventListener('submit');
-document.querySelector('form')[1].addEventListener('submit', doLogin);
+const username = document.querySelector("form")[0].addEventListener("submit");
+document.querySelector("form")[1].addEventListener("submit", doLogin);
