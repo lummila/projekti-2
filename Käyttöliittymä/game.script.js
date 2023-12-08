33d72811e5b520654b5115b2bc09b46b58933cf2
elliriskala <elli.riskala@hotmail.com>
2023-12-08 14:27:51 +0200

    Käyttöliittymä

--- Käyttöliittymä/game.script.js
+++ Käyttöliittymä/game.script.js
@@ -6,3 +6,59 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 16,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// TUTORIAL MODAL
+
+const helpModal = document.querySelector('#help-modal');
+const helpButton = document.querySelector('#help-button');
+const helpSpan = document.getElementsByClassName('close')[0];
+
+helpButton.onclick = function () {
+    helpModal.style.display = "block";
+}
+
+helpSpan.onclick = function () {
+    helpModal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == helpModal) {
+        helpModal.style.display = "none";
+    }
+}
+
+const instructionModal = document.querySelector('#instruction-modal');
+const instructionButton = document.querySelector('#instruction-button');
+const instructionSpan = document.getElementsByClassName('close')[1];
+
+instructionButton.onclick = function () {
+    instructionModal.style.display = "block";
+}
+
+instructionSpan.onclick = function () {
+    instructionModal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == instructionModal) {
+        instructionModal.style.display = "none";
+    }
+}
+
+const workModal = document.querySelector('#work-modal');
+const workButton = document.querySelector('#work-button');
+const workSpan = document.getElementsByClassName('close')[2];
+
+workButton.onclick = function () {
+    workModal.style.display = "block";
+}
+
+workSpan.onclick = function () {
+    workModal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == workModal) {
+        workModal.style.display = "none";
+    }
+}
