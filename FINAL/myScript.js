591bb02bb1950e718a2eca36d54fe72dddd8e9f8
karripartanen <karripar@metropolia.fi>
2023-12-11 02:31:57 +0200

    Karri 10.12.2023

--- FINAL/myScript.js
+++ FINAL/myScript.js
@@ -90,17 +90,132 @@ registerButton.addEventListener("click", (e) => {
 });
 
 function checkPin() {
-    // Get the PIN input value
+    // Get the PIN and username input values
     let pin = document.getElementById('query-pass').value;
+    let username = document.getElementById('query-user').value;
+    let text = document.getElementById("messageBox");
 
-    // Define a regular expression to match exactly 4 digits
-    const checker = /^\d{4}$/;
+    // Match PIN to exactly 4 digits (no letters)
+    const pinChecker = /^\d{4}$/;
 
-    // Test if the PIN matches the regular expression and does not contain letters
-    if (checker.test(pin) && !/[a-zA-Z]/.test(pin)) {
-        alert("Valid PIN: " + pin);
+    // Check if PIN and username are not empty, otherwise all gucci
+    if (pin.trim() !== "" && username.trim() !== "" && pinChecker.test(pin) && !/[a-zA-Z]/.test(pin)) {
+        text.innerText = "Credentials valid";
+        text.style.color = "green";
+        text.style.fontWeight = "900";
     } else {
-        alert("Invalid PIN. Try again.");
+        text.innerText = "Invalid credentials.";
+        text.style.color = "red";
+        text.style.fontWeight = "900";
     }
 }
+
+
+function toGame() {
+  window.location.href = "index.html"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// TÄSTÄ ETEENPÄIN KOPIOITU SCRIPT.JS:STÄ
+
+// Käyttäjän kirjoittama nimi ja PIN-koodi
+const user_username = document.querySelector("#query-user");
+const user_password = document.querySelector("#query-pass");
+
+// Kirjautumis- ja rekisteröitymisnappulat
+const login = document.querySelector(".login");
+const register = document.querySelector(".register");
+
+// Debug-tarkoituksiin
+const result = document.querySelector(".result");
+
+function update(person) {
+  document.querySelector("#next-hint").innerHTML = person.hint;
+}
+async function getLogin() {
+  try {
+    const username = user_username.value;
+    const password = user_password.value;
+
+    const response = await fetch(
+      `http://localhost:5000/login?username=${username}&password=${password}`
+    );
+    const json = await response.json();
+    console.log(json);
+    update(username);
+    return json.name;
+  } catch (error) {
+    console.error("doLogin fail", error);
+    return error;
+  }
+}
+
+async function doLogin(event) {
+  const response = await getLogin();
+  if (response) {
+    console.log(response);
+    console.log("success");
+    return response;
+  } else {
+    console.log("some Login error");
+    return response.error;
+  }
+}
+
+async function getRegister() {
+  try {
+    const username = user_username.value;
+    const password = user_password.value;
+
+    const response = await fetch(
+      `http://localhost:5000/register?username=${username}&password=${password}`
+    );
+    const json = await response.json();
+    console.log(json);
+    return json.name;
+  } catch (error) {
+    console.error("doRegister fail", error);
+    return error;
+  }
+}
+
+async function doRegister() {
+  const response = await getRegister();
+  if (response) {
+    console.log(response);
+    console.log("success");
+    update;
+    return response;
+  } else {
+    console.log("some Register error");
+    return response.error;
+  }
+}
+
+//Ongelma: miten saada formien molemmat arvot syötettyä samalle reitille?
+// const username = document.querySelector("form")[0].addEventListener("submit");
+
+login.addEventListener("click", (e) => {
+  e.preventDefault();
+  result.textContent = user_username.value + " " + user_password.value;
+  const promise = doLogin();
+});
+
+register.addEventListener("click", (e) => {
+  e.preventDefault();
+  result.textContent = user_username.value + " " + user_password.value;
+  const promise = doRegister();
+});
+
+
 
